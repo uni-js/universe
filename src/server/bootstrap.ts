a3dbb87a65e82f39c61e63a3c00c540589a89f1b
wyattzheng <854185073@qq.com>
2022-04-10 17:42:37 +0800

    feat: unhandled error will not crash the program

--- src/server/bootstrap.ts
+++ src/server/bootstrap.ts
@@ -6,3 +6,11 @@ function bootstrap() {
 }
 
 bootstrap();
+
+process.on('uncaughtException', (err) => {
+	console.error("uncaughtException" , err);
+})
+
+process.on('unhandledRejection', (err) => {
+	console.error("unhandledRejection" , err);
+})
